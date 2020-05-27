423def298142e3d1c3962d27864028669f8b7b15
Radek Piekný <pieknyradek@gmail.com>
2020-05-28 00:24:43 +0200

    skill change

--- src/app/components/skill-range/skill-range.component.ts
+++ src/app/components/skill-range/skill-range.component.ts
@@ -16,32 +16,66 @@ export class SkillRangeComponent implements OnInit {
     this.params = {
       dates: [
         {
-          date: new Date(2001,10,1),
+          date: new Date(2001,0,1),
           value: 10
         },
         {
-          date: new Date(2003,10,1),
+          date: new Date(2013,10,1),
+          value: 50
+        },
+        {
+          date: new Date(2002,0,1),
           value: 20
         }
       ],
-      startDate: new Date(2020,4,1),
+      startDate: new Date(2001,0,1),
       endDate: new Date()
     }
-    this.date = new Date(2020,4,26);
+    this.date = new Date(2001,9,1);
+    this.params = this.paramsSortByDate(this.params);
 
-    this.skillValue = this.calculateSkill(this.params.startDate, this.params.endDate,this.date);
+    //this.skillValue = this.calculateDatePositionPercentage(this.params.startDate, this.params.endDate,this.date);
+    this.skillValue = this.calculateSkill();
+    console.log(this.skillValue);
   }
 
   ngOnChanges(changes: SimpleChanges): void {
     if (changes.date) {
-      this.skillValue = this.calculateSkill(this.params.startDate, this.params.endDate,this.date);
+      this.skillValue = this.calculateDatePositionPercentage(this.params.startDate, this.params.endDate,this.date);
+    }
+    if (changes.params) {
+      this.params = this.paramsSortByDate(this.params);
     }
   }
 
-  calculateSkill(startDate: Date, endDate: Date, skillDate: Date): number {
+  paramsSortByDate(params: SkillRangeComponentParams): SkillRangeComponentParams {
+    let newDates = params.dates.sort((a,b) => {
+      return a.date.getTime() - b.date.getTime()
+    })
+    params.dates = newDates;
+    return params;
+  }
+
+  calculateDatePositionPercentage(startDate: Date, endDate: Date, skillDate: Date): number {
     let timeRange: number = endDate.getTime() - startDate.getTime();
     let currentTime: number = skillDate.getTime() - startDate.getTime()
     return currentTime / timeRange * 100;
+  }
+
+  calculateSkill(datePositionPercentage?: number): number {
+    let fromDate: SkillRangeComponentParamDate;
+    let toDate: SkillRangeComponentParamDate;
+    let i: number = this.params.dates.reverse().findIndex( d => d.date < this.date);
+    this.params.dates.reverse(); //reverse changes original array
+    i = this.params.dates.length - i - 1;
+    if (i == 0 && this.params.dates.length == 1) {
+      return this.params.dates[0].value
+    }
+    if (this.date.getTime() > this.params.dates[this.params.dates.length - 1].date.getTime()) {
+      return this.params.dates[this.params.dates.length - 1].value
+    }
+    let DatePositionPercentage: number = this.calculateDatePositionPercentage(this.params.dates[i].date,this.params.dates[i+1].date,this.date)
+    return this.params.dates[i+1].value * DatePositionPercentage / 100
   }
 
 }
