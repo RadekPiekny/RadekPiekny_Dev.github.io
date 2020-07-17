794bfffa4b94c9e0efc5f3bc572ad4027590478f
RadekPiekny <39513967+RadekPiekny@users.noreply.github.com>
2020-07-17 18:52:01 +0200

    Update lightning-canvas.component.ts

--- src/app/components/lightning-canvas/lightning-canvas.component.ts
+++ src/app/components/lightning-canvas/lightning-canvas.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { ILightning, ILine, IPoint } from 'src/app/models/lightning.model';
-import { Subject } from 'rxjs';
+import { Subject, of } from 'rxjs';
 import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+import { delay, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'lightning-canvas',
@@ -11,14 +12,16 @@ import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 export class LightningCanvasComponent implements OnInit {
   @ViewChild('canvas', {static: true}) canvas: ElementRef<HTMLCanvasElement>;
   ctx: CanvasRenderingContext2D;
-  canvasWidth: number = 300;
-  canvasHeight: number = 300;
+  canvasWidth: number = 500;
+  canvasHeight: number = 500;
 
   lightningID: number = 0;
 
   fps$: Subject<number> = new Subject<number>();
   fps: number;
   frameTime: number;
+  lightnings: ILightning[] = [];
+  paintFrame: number;
 
   constructor() { }
 
@@ -26,6 +29,7 @@ export class LightningCanvasComponent implements OnInit {
     this.ctx = this.setupCanvas(this.canvas);
     this.ctx.fillStyle = "black";
     this.ctx.fillRect(0, 0, this.canvasWidth, this.canvasHeight);
+
     this.fps$.next(this.calcFPS());
     this.fps$.subscribe(f => {
       this.fps = f;
@@ -36,69 +40,144 @@ export class LightningCanvasComponent implements OnInit {
 
   setupCanvas(canvas: ElementRef<HTMLCanvasElement>) {
     let devicePixelRatio = window.devicePixelRatio;
-    var rect = this.canvas.nativeElement.getBoundingClientRect();
+    var rect = canvas.nativeElement.getBoundingClientRect();
     canvas.nativeElement.width = this.canvasWidth *devicePixelRatio;
     canvas.nativeElement.height = this.canvasWidth *devicePixelRatio;
     canvas.nativeElement.style.width = (this.canvasWidth).toString() + "px";
     canvas.nativeElement.style.height = (this.canvasWidth).toString() + "px";
-    let canvas_context = this.canvas.nativeElement.getContext('2d');
+    let canvas_context = canvas.nativeElement.getContext('2d');
     canvas_context.scale(devicePixelRatio, devicePixelRatio);
     return canvas_context;
   }
 
-  resetToDefault() {
+  canvasPaint(frameTime?: number, i: number = 0) {
+    let currentFrameTime: number = Date.now();
+    this.paintFrame = currentFrameTime - frameTime;
+    this.ctx.fillStyle = "black";
+    this.ctx.fillRect(0, 0, this.canvasWidth, this.canvasHeight);
+    this.clear(this.lightnings);
+    for (let i = 0; i < this.lightnings.length; i++) {
+      this.drawLightningNew(this.lightnings[i]);
+    }
+    requestAnimationFrame(() => this.canvasPaint(currentFrameTime,i++));
+  }
 
+  canvasPaintOnce() {
+
+    for (let i = 0; i < this.lightnings.length; i++) {
+      this.drawLightningGlowPart(this.lightnings[i]);
+    }
+    for (let i = 0; i < this.lightnings.length; i++) {
+      this.drawLightningPart(this.lightnings[i]);
+    }
+    
+    requestAnimationFrame(() => this.canvasPaintOnce());
+  }
+
+  clear(l: ILightning[]) {
+    l.forEach((l,i) => {
+      if (l.id == -1) {
+        this.lightnings.splice(i,1);
+        this.clear(this.lightnings);
+      }
+    });
   }
 
   thunderStorm() {
-    for (let index = 0; index < 20; index++) {
-      setTimeout(() => {
-        this.drawNewLightning();
-      }, this.getRandomInt(16,3600));
+    for (let index = 0; index < 1; index++) {
+      this.generateNewLightning();
     }
   }
 
-  drawLightning(l: ILightning, lastLineIndex: number = 0) {
-    this.ctx.beginPath();
-    this.ctx.moveTo(l.line[lastLineIndex].p2.x, l.line[lastLineIndex].p2.y);
+  drawLightningPart(l: ILightning) {
+
+    let currentLastLinePaint: number = l.lastLinePaint + l.cyclePerFrame;
+    if (currentLastLinePaint >= l.line.length - 1) {
+      l.lastLinePaint = l.line.length - 1;
+    } 
+
+    this.ctx.filter = "none"
     this.ctx.strokeStyle = "rgba(255,255,255,1)";
-
-    this.ctx.lineTo(l.line[lastLineIndex + 1].p2.x, l.line[lastLineIndex + 1].p2.y);
-    this.ctx.lineWidth = l.line[lastLineIndex + 1].width;
     this.ctx.lineCap = "round";
+    for (let i = l.lastLinePaint; i < currentLastLinePaint; i++) {
+      this.ctx.moveTo(l.line[i].p1.x, l.line[i].p1.y);
+      this.ctx.lineTo(l.line[i].p2.x, l.line[i].p2.y);
+      this.ctx.lineWidth = l.line[i].width;
+    }
     this.ctx.stroke();
-    this.ctx.closePath();
-    this.drawShadow((l.line[lastLineIndex + 1].p1.x+l.line[lastLineIndex + 1].p2.x)/2,(l.line[lastLineIndex + 1].p1.y+l.line[lastLineIndex + 1].p2.y)/2,l.line[lastLineIndex + 1].width*8);
+  }
 
-    if (lastLineIndex == l.line.length - 2) {
+  drawLightningGlowPart(l: ILightning) {
+    let currentLastLinePaint: number = l.lastLinePaint + l.cyclePerFrame;
+    if (currentLastLinePaint >= l.line.length - 1) {
+      l.lastLinePaint = l.line.length - 1;
+    } 
+    let grad = this.ctx.createLinearGradient(l.pointOrigin.x, l.pointOrigin.y,l.line[l.lastLinePaint].p2.x,l.line[l.lastLinePaint].p2.y);
+    grad.addColorStop(0,"rgba(0,0,255,1)");
+    grad.addColorStop(1,"rgba(255,255,255,1)");    
+
+    for (let i = l.lastLinePaint; i < currentLastLinePaint; i++) {
+      this.ctx.beginPath();
+      this.ctx.moveTo(l.line[i].p1.x, l.line[i].p1.y);
+      this.ctx.lineTo(l.line[i].p2.x, l.line[i].p2.y);
+      this.ctx.lineWidth = l.line[i].width*12;
+      this.ctx.strokeStyle = "rgba(255,255,255,0.3)";
+      
+      //this.ctx.filter = "blur(5px)"; //performance issues hardcore
+    }
+    this.ctx.stroke();
+    l.lastLinePaint = currentLastLinePaint;
+  }
+
+  drawLightningNew(l: ILightning) {
+    this.ctx.beginPath();
+    this.ctx.filter = "none"
+    
+    this.ctx.strokeStyle = "rgba(255,255,255,1)";
+    this.ctx.lineCap = "round";
+
+    let currentLastLinePaint: number = l.lastLinePaint + l.cyclePerFrame;
+    if (currentLastLinePaint >= l.line.length - 1) {
+      l.lastLinePaint = l.line.length - 1;
+      if (Date.now() - l.startTime > l.duration) {
+        l.id = -1;
+        return;
+      }
+    } 
+    this.drawLightningGlow(l);
+
+    this.ctx.filter = "none"
+    this.ctx.beginPath();
+    for (let i = 0; i < l.lastLinePaint; i++) {
+      
+      this.ctx.moveTo(l.line[i].p1.x, l.line[i].p1.y);
+      this.ctx.lineTo(l.line[i].p2.x, l.line[i].p2.y);
+      this.ctx.lineWidth = l.line[i].width;
+      this.ctx.stroke();
+    }
+    this.ctx.closePath();
+    l.lastLinePaint = currentLastLinePaint;
+  }
+
+  drawLightningGlow(l: ILightning) {
+    if (l === undefined) {
       return;
     }
-    lastLineIndex++;
+    let grad = this.ctx.createLinearGradient(l.pointOrigin.x, l.pointOrigin.y,l.line[l.lastLinePaint].p2.x,l.line[l.lastLinePaint].p2.y);
+    grad.addColorStop(0,"rgba(200,200,255,0.15)");
+    grad.addColorStop(1,"rgba(255,255,255,0.15)");    
 
-    let framesCount: number = l.animationDuration / this.frameTime;
-    let cyclePerFrame: number = Math.ceil(l.line.length / framesCount);
-    if (lastLineIndex % cyclePerFrame == 0) {
-      requestAnimationFrame(() => this.drawLightning(l,lastLineIndex));
-    } else {
-      this.drawLightning(l,lastLineIndex);
+    for (let i = 0; i < l.lastLinePaint; i++) {
+      this.ctx.beginPath();
+      this.ctx.moveTo(l.line[i].p1.x, l.line[i].p1.y);
+      this.ctx.lineTo(l.line[i].p2.x, l.line[i].p2.y);
+      this.ctx.lineWidth = l.line[i].width*8;
+      this.ctx.strokeStyle = grad;
+      this.ctx.stroke();
+      
+      this.ctx.filter = "blur(5px)"; //performance issues hardcore
     }
-  }
-
-  drawNewLightning(l?: ILightning) {
-    this.resetToDefault();
-    this.drawLightning(l ? l : this.generateDefaultLightning());
-  }
-
-  drawShadow(x: number, y: number, r: number) {
-
-    let grad = this.ctx.createRadialGradient(x,y,0,x,y,r);
-    grad.addColorStop(0,"rgba(255,255,255,0.1)");
-    grad.addColorStop(1,"rgba(255,255,255,0)");
-    this.ctx.fillStyle = grad;
-    this.ctx.arc(x,y,r,0, 2 * Math.PI);
-    this.ctx.fill();
-    //this.ctx.closePath();
-    this.ctx.filter = "none";
+    this.ctx.closePath();
   }
 
   getRandomIntNegativeNumber(min, max) {
@@ -109,22 +188,37 @@ export class LightningCanvasComponent implements OnInit {
     if (!l) {
       l = this.generateDefaultLightning();
     }
+    this.lightnings.push(l);
+    /*setTimeout(() => {
+      this.lightnings.splice(this.lightnings.findIndex(lightning => lightning.id == l.id),1);
+    }, l.duration);*/
   }
 
   generateDefaultLightning(): ILightning {
+    let _animationDuration: number = this.getRandomInt(60,180);
+
     let l: ILightning = {
       id: this.lightningID++,
       widthStart: 3,
       widthEnd: 0.3,
-      pointOrigin: {x: this.getRandomArbitrary(0,300), y: 0},
+      pointOrigin: {x: this.getRandomArbitrary(20,60), y: 0},
       tendencyHorizontal: {left: -3, right: 6},
-      tendencyVertical: {top: 5, bottom: 5},
-      lineCount: this.getRandomInt(15,60),
+      tendencyVertical: {top: 1, bottom: 8},
+      lineCount: this.getRandomInt(60,60),
       line: null,
-      animationDuration: this.getRandomInt(150,300),
-      lightningChain: null
+      animationDuration: _animationDuration,
+      lastLinePaint: 0,
+      lightningChain: null,
+      channelAnimation: true,
+      duration: _animationDuration + 5000,
+      cyclePerFrame: null,
+      startTime: Date.now()
     };
+
     l.line = this.generateLinesLightning(l);
+    let framesCount: number = l.animationDuration / this.frameTime;
+    let cyclePerFrame: number = Math.ceil(l.line.length / framesCount);
+    l.cyclePerFrame = cyclePerFrame;
 
     return l;
   }
